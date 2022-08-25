a6ca713a8f0ed99d6eba67ae0014e413ca185808
504 <504@DESKTOP-A1HFHNV>
2022-08-25 21:26:18 +0900

    0825

--- front2/public/JS/함수/15.함수예제.js
+++ front2/public/JS/함수/15.함수예제.js
@@ -1,24 +1,23 @@
-/**
- * 
- */alert('실행')
- 
- //p204
- function sumAll( a,b){
-	let output = 0
-	for(let i = a ; i<=b ; i++){
-		output += i
-	}
-	return output
-}
-console.log(`1부터 100까지의 합 : ${sumAll(1 , 100 )}`)
-console.log(`1부터 500까지의 합 : ${sumAll(1 , 500 )}`)
+//204p
 
-//계산하기 버튼눌림 함수
-function 계산버튼함수(){
-	alert('버튼눌림성공')
-	let snumber = document.activeElementId('sinput').value
-	let enumber = document.activeElementId('einput').value
-	let result = sumAll( snumber , enumber )
-	document.activeElementId('resultbox')
+function sumAll(a,b){
+	// sumAll 이름으로 함수명 선언, 매개변수 a , b 
+		// 매개변수 : 외부에서 함수 호출시 들어오는 값[데이터]
+		let output = 0 //변수선언
+		for(let i = a ; i<=b ; i++){
+			output += i
+		}
+		return output
 }
-alert('snum')
+
+ console.log(`1부터 100까지의 합 : ${sumAll(1 , 100)}`)
+ console.log(`1부터 500까지의 합 : ${sumAll(1 , 500)}`)
+ 
+ //계산하기 버튼을 눌렀을때 함수
+ function 계산버튼함수(){
+	alert('버튼눌림 성공')
+	let snumber = Number(document.getElementById('sinput').value)
+	let enumber = Number(document.getElementById('einput').value)
+	let result = sumAll(snumber , enumber)
+	document.getElementById('resultbox').innerHTML = '계산결과 : '+result
+}
--- front2/public/JS/함수/16.틱택톡.js
+++ front2/public/JS/함수/16.틱택톡.js
@@ -1,50 +1,41 @@
-/**
-
+/*
 	1. 9개 버튼 생성 [ 게임판 변수에 모든 버튼를 저장한다. ]
 		1. 3개마다 줄바꿈처리  2 5 8 일때 
 	2. 버튼 클릭하면 모양 변경 
 		1. 선택한 버튼의 모양 변경 [ O ]
 		2. [컴퓨터]난수 발생 모양 변경 [ X ]
-	3. 근접한 버튼의 모양이 3개 모두 동일하면 승리 
-
+	3. 근접한 버튼의 모양이 3개 모두 동일하면 승리  
 */
-//삼목만들기
-
 
 let 게임상황 = [] //현재 알을 둔 상황 저장배열
 let 승리 ='' //이긴알을 저장하는 함수
 
-//게임판 버튼 만드는 함수
+//1.게임판 버튼 만드는 함수
 function 게임판그리기(){
-	
 	승리 = ''
 	게임상황 = [null,null,null,null,null,null,null,null,null]
-	let 게임판 =''
+	let 게임판 = ''
 	
-	for(let i = 0 ; i<9 ; i++){
-		게임판 += `<button id=${i} oneclick="버튼선택(${i})">${i}</button>`
-		if( i % 3 == 2){게임판 +=`<br>`}
+	for( let i = 0 ; i <9 ; i++){
+		게임판 += `<button id=${i} onclick="버튼선택(${i})">${i}</button>`
+		if( i % 3 == 2){게임판 += `<br>`}
 	}
 	document.getElementById('gameboard').innerHTML = 게임판
 }
 
-//버튼눌렸을때 이벤트 함수
-function 버튼선택(i){
+//2.버튼 눌렀을떄 이벤트 함수
+ function 버튼선택(i){
 	if(중복검사(i)){
-		alert('안내) 이미 둔 자리입니다')
+		alert('안내)이미 둔 자리입니다')
 		return
 	}
-
-
 	document.getElementById(i).innerHTML = 'O'
 	게임상황[i] = 'O'
 	
-	if( 게임결과( ) ) { // 게임결과 함수에서 true가 리턴되면 
-		alert('게임종료 승리 : ' + 승리 )
-		return // 함수 강제 종료 
+	if(게임결과( )){
+		alert('게임종료 승리 : ' +승리)
+		return
 	}
-	
-	
 	//난수발생
 	while(true){
 		let rand = Math.floor(Math.random()*9)
@@ -54,37 +45,28 @@ function 버튼선택(i){
 			break
 		}
 	}
-
 	if(게임결과( )){
-		alert('게임종료 승리 : ' + tmdfl)
+		alert('게임종료 승리 : ' + 승리)
 		return
 	}
-	}
-	
-	//현재의치에 알이 있는지 검사함수
-function 중복검사( i ){
+}
+
+ //3.현재 위치에 알이 있는지 검사 함수[중복검사]
+ function 중복검사(i){
 	if(게임상황[i] != null){
-		return true
+		return true //알이 있다
 	}else{
-		return false
+		return false //알이 없다 null이면
 	}
 }
-	
-	//현재 상황에 게림결과 판단하는 함수
-function 게임결과(){
-	for(let i = 0 ; i<6 ; i=3){
-		if(게임상황[i] !=null && 게임상황[i] == 게임상황[i+1] == 게임상황[i+1] == 게임상황[i+2]){
-			승리 = 게임상황[i] //가로로 이기는 함수
-			return true
-		}
-	}
-
-
-	//세로로 이기는 함수
-	for(let i = 0 ; i<=2 ; i++){
-		if(게임상황[i] !=null && 게임상황[i] == 게임상황[i+3] == 게임상황[i+3] == 게임상황[i+6]){
+//4.현재 상황에 겜임결과 판단하는 함수
+ function 게임결과(){
+	for(let i = 0 ; i<=2 ; i+=3){
+		//i는 0부터 6까지 3씩 증가반복[3회 : 0 3 6]
+		if(게임상황[i] != null && 게임상황[i] == 게임상황[i+1] == 게임상황[i+2]){
+			//i가 0일때 0==1 ==2 i가 3일때 3==4==5 i가 6일때 6==7==8
 			승리 = 게임상황[i]
 			return true
 		}
 	}
-	}
+}
