e650530a6a9b508b00cdd396525575286a89b68e
504 <504@DESKTOP-A1HFHNV>
2022-08-30 21:20:45 +0900

    0830

--- front2/public/CSS/웹디자인/챕터4/스크립트.js
+++ front2/public/CSS/웹디자인/챕터4/스크립트.js
@@ -1,6 +1,8 @@
 /**
  * 차트리스트.js 이용한 차트구현
  */
+ 
+  let 지원자리스트 = [ ]//지원자 객체들을 담을 배열 선언
 //파이차트(원형)=백분율 데이터객체 선언
  let pieData = {
 	labels : ['웹디자이너' , '웹개발자' , '서버엔지니어' , '영업직'],// 항목의 이름
@@ -45,7 +47,7 @@ new Chartist.Bar('.bar_chart',barData,barOptions)
 		offset : 60 ,
 		scaleMinSpace:50 ,
 		labelInterpolationFnc: function(value){
-			return value + '명'
+			return value + '원'
 		}
 	},
 	width : '100%',
@@ -59,3 +61,32 @@ function button(){
  
  new Chartist.Bar('.bar_chart2',barData2,barOptions2)
 }
+
+
+
+//지원자 목록저장
+
+ function 지원자등록(){
+	console.log('지원자 등록')//1번 테스트
+	let 입력리스트 = document.querySelectorAll('input')
+	let 목록상자 = document.querySelector('select')
+	let 긴글상자 = document.querySelector('textarea')
+	//2번테스트
+	console.log(입력리스트)
+	console.log(목록상자)
+	console.log(긴글상자)
+	
+	let 지원자 = {
+		성명 : 입력리스트[0].value,
+		영문명 : 입력리스트[1].value,
+		메일주소 : 입력리스트[2].value,
+		포토폴리오 : 입력리스트[3].value,
+		희망근무지 : 입력리스트[4].value,
+		현재상태 : 목록상자.value,
+		자기소개 : 긴글상자.value,
+	}
+	console.log(지원자)
+	지원자리스트.push(지원자)
+	console.log(지원자리스트)
+}
+
---
+++ front2/public/CSS/웹디자인/챕터4/지원자목록.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+ function 지원자등록(){
+	console.log('지원자 등록')//1번 테스트
+	let 입력리스트 = document.querySelectorAll('input')
+	let 목록상자 = document.querySelectorAll('select')
+	let 긴글상자 = document.querySelectorAll('textarea')
+	//2번테스트
+	console.log(입력리스트)
+	console.log(목록상자)
+	console.log(긴글상자)
+}
